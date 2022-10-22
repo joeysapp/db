77c664349ef7eb10104a32456c187c10b533858f
joeysapp <sapp.joey@gmail.com>
2022-10-21 23:20:27 -0500

    [node] Listening to spa

--- nodemon/server.js
+++ nodemon/server.js
@@ -3,32 +3,28 @@
 // |     |  _  |  _  ||  -__|
 // |__|__|_____|_____||_____|
 // ================================================================================
-// https://github.com/websockets/ws
 // https://www.npmjs.com/package/pg
-// https://github.com/mongodb/node-mongodb-native
-
-import { createServer } from 'http';
-import { parse } from 'url';
-import { WebSocketServer } from 'ws';
-
 
 //         __   __ __
 // .--.--.|  |_|__|  |.-----.
 // |  |  ||   _|  |  ||__ --|
 // |_____||____|__|__||_____|
 // ========================================
-const toMsg = (msg, {
-  action: '',
-  type: 'log',
-  action: '',
-
-  to: '',
-  from: '',
-
-  timestamp: new Date(),
-} => {
-  return JSON.stringify({ msg, type, action, timestamp });
+// (probably in another file)
+// ========================================
+// Parse out JSON buffer into object, coming from sockets
+const parseMessage = (jsonBuffer) => {
+  return JSON.parse(jsonBuffer);
 }
+// Format object to string to send to sockets
+const formatMessage = (_message, {
+  message,
+  action = '',
+  type = '',
+  to = '',
+  from = '',
+  timestamp = new Date(),
+} = {}) => JSON.stringify({ message: _message || message, type, action, timestamp });
 
 
 
@@ -38,24 +34,36 @@ const toMsg = (msg, {
 // |  |  |  |__ --|__ --|
 // |________|_____|_____|
 // ========================================
+// (think about implementing wss via c with basic networking)
+// ========================================
+
+// https://github.com/websockets/ws
+import { createServer } from 'http';
+import { parse } from 'url';
+import { WebSocketServer } from 'ws';
+// const createServer = require('http');
+// const WebSocketServer = require('ws');
+
 const wss = new WebSocketServer({ noServer: true, perMessageDeflate: false });
 wss.on('connection', (ws, req) => {
 
-  console.log('connection (ws, req)', ws, req);
-  const greeting = toMsg(
+  // console.log('connection (ws, req)', ws, req);
+  const greeting = formatMessage(
     'WSS: Greetings',
     { type: 'log', to: 'client', from: 'server' });
-  ws.send(toMsg('WSS: Greetings ', ));
+
+  ws.send(greeting);
 
   // Receiving stuff
   ws.on('message', jsonBuffer => {
-    let msg = {};
+    let message = {};
     try {
-      msg = toMsg(JSON.parse(jsonBuffer));
+      message = parseMessage(jsonBuffer);
     } catch {
-      msg = toMsg('Error parsing JSON Buffer object', { type: 'error' });
+      message = formatMessage('Error parsing JSON Buffer object', { type: 'error' });
     }    
-    console.log('on message (jsonBuffer)', msg);
+    console.log('on message (jsonBuffer)');
+    console.dir(message, { depth: null, colors: true });
   });
 
   // Closing
@@ -72,16 +80,19 @@ wss.on('connection', (ws, req) => {
 // ========================================
 const server = createServer();
 server.on('upgrade', (request, socket, head) => {
+  wss.handleUpgrade(request, socket, head, (ws) => {
+    wss.emit('connection', ws, request);
+  });
 
-  // Pathname could be used for different behavior / multiple WSS at once
-  const { pathname } = parse(request.url);
-  if (pathname === '/') {
-    wss.handleUpgrade(request, socket, head, (ws) => {
-      wss.emit('connection', ws, request);
-    });
-  } else {
-    socket.destroy();
-  }
+  // Pathname could be used for different behavior / multiple WSS at once (require(url))
+  // const { pathname } = parse(request.url);
+  // if (pathname === '/') {
+  //   wss.handleUpgrade(request, socket, head, (ws) => {
+  //     wss.emit('connection', ws, request);
+  //   });
+  // } else {
+  //   socket.destroy();
+  // }
 
   // Auth
   // This function is not defined on purpose. Implement it with your own logic.
